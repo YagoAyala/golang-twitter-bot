0b633a3b3b391ed5de82389fd5d714f0483a7368
YagoAyala <yago.ayala@outlook.com>
2021-09-25 13:32:00 -0300

    feat: created a bot using golang

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
+)
+
+	func main() {
+		godotenv.Load()
+
+		config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET_KEY"))
+		token := oauth1.NewToken(os.Getenv("TOKEN_KEY"), os.Getenv("TOKEN_SECRET_KEY"))
+		httpClient := config.Client(oauth1.NoContext, token)
+	
+		// Client do twitter
+		client := twitter.NewClient(httpClient)
+	
+		// Publicar um tweet
+		_, _, err := client.Statuses.Update("uma mensagem f", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		// Procurar por tweets
+		tweets, _, err := client.Search.Tweets(&twitter.SearchTweetParams{
+			Query: "#golang",
+			Count: 5,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		// Verificar todos os tweets listados
+		for _, val := range tweets.Statuses {
+			log.Print("Nome do usuario :", val.User.Name)
+			log.Print("Tweet :", val.Text)
+	
+		// Retweetar os tweets listados
+		_, _, err := client.Statuses.Retweet(val.ID, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+	}
+}
